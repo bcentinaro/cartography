54af83d90b964d611bfa304ebdbc8716b2fa527f
Bill <bill@theresnobox.net>
2012-09-13 10:45:37 -0400

    Added new files

--- lib/cartography/sitemap.rb
+++ lib/cartography/sitemap.rb
@@ -4,15 +4,22 @@ module Cartography
       @options = {}
       @options.merge(options)
       @baseUrl = baseUrl
-      @urls = {}
+      @urls = []
     end
 
+    # Returns an XML string of the sitemap
     def to_s
       return ''
     end
 
+    # writes an XML string of the sitemap to the specified file.
     def to_file(path)
       return 
+    end
+
+    #Adds a url to the array of urls.
+    def add(url, lastmod, changefreq, priority)
+      @urls << {:url => url, :lastmod => lastmod, :changefreq => changefreq, :priority => priority}
     end
   end
 end
